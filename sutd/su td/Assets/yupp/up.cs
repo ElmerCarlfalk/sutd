ae145273cf2378ea8a3b1ee9a7bad71d9dc61637
Anton <anton.stenum@elev.ga.lbs.se>
2021-08-27 12:12:22 +0200

    up

--- sutd/su td/Assets/yupp/up.cs
+++ sutd/su td/Assets/yupp/up.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class yup : MonoBehaviour
+public class up : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
