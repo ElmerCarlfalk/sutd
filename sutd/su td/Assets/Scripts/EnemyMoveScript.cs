5a67fa4d9d87401360486f7c90bb920c4ec2e018
Jonathanhillden <74252905+Jonathanhillden@users.noreply.github.com>
2021-09-03 12:23:56 +0200

    Enemy spawner
    Note: Enemies kan röra vid varandra

---
+++ sutd/su td/Assets/Scripts/EnemyMoveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 25f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(speed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+}
---
+++ sutd/su td/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform spawnPoint;
+    float spawnRange; 
+    public float spawnRate = 2f;  // Mellanrum mellan spawns
+    float nextSpawn = 0.0f;  //variabel för nästa spawn
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spawnRange = Random.Range()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            Instantiate(enemy, spawnPoint);
+        }
+    }
+}
