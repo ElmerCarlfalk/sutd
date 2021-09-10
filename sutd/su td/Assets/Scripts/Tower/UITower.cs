58d1af870d89b98de20ef20b12e32ea97b041d6d
Anton <anton.stenum@elev.ga.lbs.se>
2021-09-10 11:34:09 +0200

    BuyScreen
    Added a BuyScreen
    Added a BuyTower Button
    Added a way to buy things (no currency)
    Added a temp Tower
    Added a temp Tower Script
    Fixed Play Button

---
+++ sutd/su td/Assets/Scripts/Tower/UITower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITower : MonoBehaviour
+{
+    bool placed = false;
+
+    private void Update()
+    {
+        if (!placed)
+        {
+            if (!Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            else
+            {
+                placed = true;
+            }
+        }
+        else
+        {
+        }
+    }
+}
---
+++ sutd/su td/Assets/Scripts/UI/Menu/BuyButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyButton : MonoBehaviour
+{
+    public GameObject Tower;
+    Vector3 mousePosition;
+
+    public void Start()
+    {
+        mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    public void ClickBuyButton()
+    {
+        Instantiate(Tower, mousePosition, Quaternion.identity);
+    }
+}
